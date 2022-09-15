d9e2a33554c2374df5552564b61c83fc4cd92bbf
wenbin1354 <wblin2986@gmail.com>
2022-09-15 13:47:54 -0400

    added tictactoe for 2 player

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+
+
+const htmlContainer = document.getElementById("root");
+const root = ReactDOM.createRoot(htmlContainer);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+
+
+
+
+
+
